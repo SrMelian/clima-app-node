d8e9b6c209e20339bca182b3fb95f09544a28970
SrMelian <manu9898@gmail.com>
2018-06-06 21:25:03 +0100

    Primer commit

---
+++ clima/clima.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+const getClima = async(lat, lng) => {
+    let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=22f547d0029ddca9b74ba57b138ee32d`);
+    
+    if (!resp) {
+        throw new Error('No hay resultados de temperatura');
+    }
+
+    return resp.data.main.temp;
+}
+
+module.exports = {
+    getClima,
+}
